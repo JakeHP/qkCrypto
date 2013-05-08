4410c66f852353a6562857f868686d8715861b6f
Jake Prather <jakeh.prather@gmail.com>
2013-05-08 11:54:25 -0500

    init

---
+++ qkCommChannel.py
@@ -0,0 +1,6 @@
+__author__ = 'Jakehp'
+
+########################## PURPOSE ##########################
+# Connects a sender and a receiver
+# Also allows for any eavesdroppers/man in the middle
+##########################################################
---
+++ qkPhoton.py
@@ -0,0 +1,14 @@
+__author__ = 'Jakehp'
+
+########################## DATA ##########################
+#Contains random bit that is inaccessible
+#Contains randomly decided basis
+#Contains polarizations for each photon (encoded randomly generated bit)
+##########################################################
+
+########################## FUNCTIONS ##########################
+#
+#
+#
+#
+###############################################################
