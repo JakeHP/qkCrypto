4318c7858ca1a8d4856917ad025cb2472e95b0ae
Jake Prather <jakeh.prather@gmail.com>
2013-06-03 21:54:57 -0500

    arch update - final sub bit string comparisons+decisions

--- qkTester.py
+++ qkTester.py
@@ -3,6 +3,10 @@ import qkSender
 import qkCommChannel
 import qkReceiver
 
+    #Amount of similar bits required to be considered a shared key
+
+similar_bit_req = 50
+
     #Entity Creation
 
 Alice = qkSender.qkSender()
@@ -10,7 +14,7 @@ insecureChannel = qkCommChannel.qkCommChannel()
 Bob = qkReceiver.qkReceiver()
 #Eve = qkAttacker.qkAttacker()
 
-    #Creation, sending, attacking and receiving of photons
+    #Creation, sending, attacking and receiving+measuring of photons
 
 Alice.createPhotonPulse()
 
@@ -29,7 +33,26 @@ Bob.compareBasis()
 Bob.sendBasis(insecureChannel, Bob.senderBasis.copy())
 Alice.checkReceiverBasis(insecureChannel)
 
-    #Alice and bob compare basis to see if a certain # of shared bits was reached. 50
+    #Alice and bob compare basis to see if a certain # of shared bits was reached.
+
+#Alice.dropInvalidPhotons()
+#Bob.dropInvalidPhotons()
+
+    #Alice and Bob compare sub set of the bit string (photon polarizations -> bits)
+
+#Alice.sendSubBitString(insecureChannel,)
+#Bob.sendSubBitString(insecureChannel,)
+#Alice.getSubBitString(insecureChannel,)
+#Bob.getSubBitString(insecureChannel,)
+
+    #Alice and Bob agree or disagree
+
+#Alice.sendDecision(insecureChannel)
+#Bob.sendDecision(insecureChannel)
+#Alice.getDecision(insecureChannel)
+#Bob.getDecision(insecureChannel)
+
+    #Alice and Bob can now try again in a new channel or use the shared key based on the decision
 
 Alice.printAll()
 insecureChannel.printAll()
